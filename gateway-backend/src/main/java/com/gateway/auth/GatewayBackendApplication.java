321e5351773034f24b5613a2cc92b57ff9eaca83
KALEESWARAN S  [CyberSecurity] <skaleeswaran00@gmail.com>
2025-07-13 17:25:14 +0000

    🚀 Add main application class

---
+++ gateway-backend/src/main/java/com/gateway/auth/GatewayBackendApplication.java
@@ -0,0 +1,11 @@
+package com.gateway.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayBackendApplication.class, args);
+    }
+}
