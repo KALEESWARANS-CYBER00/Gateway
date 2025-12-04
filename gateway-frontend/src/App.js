1341da5c0a2eb158af5140df123eb4d3e1c3fcb7
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-12-04 14:54:28 +0530

    Update routes for new auth pages and dashboard

--- gateway-frontend/src/App.js
+++ gateway-frontend/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './auth/Login';
 import Register from './auth/Register';
+import VerifyEmail from './auth/VerifyEmail';
+import ForgotPassword from './auth/ForgotPassword';
+import ResetPassword from './auth/ResetPassword';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 import { AuthProvider } from './AuthContext';
 import PrivateRoute from './PrivateRoute';
 
@@ -14,11 +18,22 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
           <Route
             path="/home"
             element={
               <PrivateRoute>
-                <Home />
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
               </PrivateRoute>
             }
           />
